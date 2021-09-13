3f2d539af63d643be819370f2f4c88050188c133
ahmedKamal-eng <ahmedkamal17781@yahoo.com>
2021-09-14 01:54:20 +0300

    first commit

---
+++ lib/shared/styles/themes.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'colors.dart';
+
+ThemeData darkTheme = ThemeData(
+  primarySwatch: defaultColor,
+  scaffoldBackgroundColor: HexColor("333739"),
+  appBarTheme: AppBarTheme(
+      backwardsCompatibility:
+          false, //to change the status bar color & brightness
+      systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: HexColor("333739"),
+          statusBarIconBrightness: Brightness.light),
+      backgroundColor: HexColor("333739"),
+      elevation: 0,
+      titleTextStyle: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+      iconTheme: IconThemeData(color: Colors.white)),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: defaultColor,
+      unselectedItemColor: Colors.grey,
+      elevation: 20,
+      backgroundColor: HexColor("333739")),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+        fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+  ),
+  fontFamily: 'Jannah',
+);
+
+ThemeData lightTheme = ThemeData(
+  primarySwatch: defaultColor,
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: AppBarTheme(
+      titleSpacing: 20,
+      backwardsCompatibility:
+          false, //to change the status bar color & brightness
+      systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+          statusBarIconBrightness: Brightness.dark),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      titleTextStyle: TextStyle(
+        color: Colors.black,
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+      iconTheme: IconThemeData(color: Colors.black)),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: defaultColor,
+      unselectedItemColor: Colors.grey,
+      elevation: 20,
+      backgroundColor: Colors.white),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(
+        fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+  ),
+  fontFamily: 'Jannah',
+);
